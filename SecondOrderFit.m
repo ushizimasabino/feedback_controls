f9894b4d2f04e879d21d3f1f95a0c5912b43b9de
Ushizima Sabino, I <60636499+ushizimasabino@users.noreply.github.com>
2022-11-16 17:54:45 -0500

    beta complete

--- SecondOrderFit.m
+++ SecondOrderFit.m
@@ -11,7 +11,7 @@ function [w,zeta,sigma] = SecondOrderFit(varargin)
 % 
 % [w,zeta,sigma] = SecondOrderFit(time, amplitude) uses linear & 
 % exponential fit to estimate the natural frequency, damping ratio, and 
-% real part from two column vectors with time and amplitude data
+% real part from two column vector arrays with time and amplitude data
 %               time(nx1)           amplitude(nx1)
 %
 % [w,zeta,sigma] = SecondOrderFit(timestep, data) uses linear & 
@@ -21,22 +21,38 @@ function [w,zeta,sigma] = SecondOrderFit(varargin)
 %               timestep = # (s)    data(nx1)
 %
 % w [=] rad/s
+%
+% REQUIRES CURVE FITTING TOOLBOX
 % =========================================================================
 % [w,zeta,sigma] = SecondOrderFit(varargin)
 
 if varargin == 1
     data = varargin{1};
-    t = data(:,1);
     y = data(:,2);
-
-    [peaks, locations] = findpeaks(y);
-    n = 1:1:size(locations,1);
-    p = polyfit(n,locations,1);
-
+    t = data(:,1);
     timestep = t(2,1) - t(1,1);
-    T = p(1) * timestep;
-    w = 2*pi/T;
-    zeta = 
 elseif isa(varargin{1},"double")
+    timestep = varargin{1};
+    data = varargin{2};
+    y = data(:,1);
 else 
+    t = varargin{1};
+    y = varargin{2};
+    timestep = t(2,1) - t(1,1);
+end
+
+[peaks, locations] = findpeaks(y);
+n = 1:1:size(locations,1);
+p = polyfit(n,locations,1);
+
+T = p(1) * timestep;
+w = 2*pi/T;
+
+nt = n*timestep;
+envelope = fit(nt,peaks,"exp2");
+sigma = - envelope.b;
+
+zeta = sigma/w;
+if zeta > 1
+    zeta = 1;
 end
